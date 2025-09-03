c7b75886b68c3611f2c1b2d5cc3083daaf27ae08
frapama6 <izgngr@gmail.com>
2025-09-03 19:36:34 +0200

    Python warning and MultiDir drive parsing issue fixed

--- gooey/gui/components/widgets/core/chooser.py
+++ gooey/gui/components/widgets/core/chooser.py
@@ -140,10 +140,11 @@ class MultiDirChooser(Chooser):
         # Remove volume labels from Windows paths
         if 'nt' == os.name:
             for i, path in enumerate(paths):
+
                 if path:
                     parts = path.split(os.sep)
                     vol = parts[0]
-                    drives = re.match(r'.*\((?P<drive>\w:)\)', vol)
+                    drives = re.match(r'.*(?P<drive>\w:)', vol)
                     paths[i] = os.sep.join([drives.group('drive')] + parts[1:])
 
         return os.pathsep.join(paths)
